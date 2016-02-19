0d788521e3b2efcd860f86eef387ab172f514085
zhouyg <yunge@taovip.com>
2016-02-19 11:59:45 +0800

    接入模拟数据，输入事件。

--- front/app/components/command-input.js
+++ front/app/components/command-input.js
@@ -2,5 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName:'p',
-  classNames:['command-input-box']
+  classNames:['command-input-box'],
+
+  keyDown(e){
+    //enter
+    if(e.keyCode === 13){
+
+      var inputCommand = this.get('inputCommand');
+
+      this.set('directory',inputCommand)
+
+      //request
+
+    }
+  },
+  actions:{
+    commandInput(value){
+
+      this.set('inputCommand',value);
+    }
+  }
 });
--- front/app/routes/index.js
+++ front/app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model(){
+		return [{
+			machineName:'110.2.34.3',
+			directoryName:'root',
+			userName:'zyg',
+			stduot:'hahahhaah',
+		},{
+			machineName:'110.2.34.3',
+			directoryName:'admin',
+			userName:'zyg',
+			stduot:'zzzddd',			
+		}];
+	}
 });
